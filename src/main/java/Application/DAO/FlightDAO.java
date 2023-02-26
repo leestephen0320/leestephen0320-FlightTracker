f41a8e58af3ed56bf9dc2463ad662d56274f145f
leestephen0320 <leestephen0320@gmail.com>
2023-02-26 05:30:03 +0000

    modified:   src/main/java/Application/DAO/FlightDAO.java

--- src/main/java/Application/DAO/FlightDAO.java
+++ src/main/java/Application/DAO/FlightDAO.java
@@ -66,7 +66,7 @@ public class FlightDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "change me";
+            String sql = "SELECT * FROM flights WHERE flight_id = "+id+";";
             
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
